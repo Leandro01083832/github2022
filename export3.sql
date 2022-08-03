188056a57c9b9d65966575ce17b7f9cb1b87a2f8
Leandro Luis da Silva <108497430+Leandro01083832@users.noreply.github.com>
2022-08-02 22:27:45 -0300

    criação de tabela fornecedor

---
+++ export3.sql
@@ -0,0 +1,18 @@
+insert into tab_produto (cod_produto, nome_propduto, preco_produto, validade_produto) values (1, 'produto1', 120.00, 2022/08/02);
+insert into tab_produto (cod_produto, nome_propduto, preco_produto, validade_produto) values (2, 'produto2', 120.00, 2022/08/02);
+
+insert into tab_cidade (cod_cidade, nome_cidade, estado_cidade) values (1, 'Cataguases', 'MG');
+insert into tab_cidade (cod_cidade, nome_cidade, estado_cidade) values (2, 'Juiz de Fora', 'MG');
+insert into tab_cidade (cod_cidade, nome_cidade, estado_cidade) values (3, 'Leopoldina', 'MG');
+insert into tab_cidade (cod_cidade, nome_cidade, estado_cidade) values (4, 'ÚBA', 'MG');
+insert into tab_cidade (cod_cidade, nome_cidade, estado_cidade) values (5, 'barbacena', 'MG');
+insert into tab_cidade ( nome_cidade, estado_cidade) values ( 'Campos', 'RJ');
+
+
+insert into tab_fornecedor ( nome_fornecedor, email_fornecedor, contato_fornecedor, id_endereco_fornecedor) values ( 'Jose', 'jose@gmail.com', '1234-1234', 12);
+
+Select * from tab_cidade;
+Select * from tab_produto;
+Select * from tab_fornecedor;
+
+
